51e6d048474d3e4ad66dd5e98b39a31a32d02127
Bojan <bojan.pejic@syrmia.com>
2021-09-24 13:27:44 +0200

    gtest

---
+++ gtest/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: //home/bojan/Desktop/unitTests/gtest-demo/test
+# Build directory: //home/bojan/Desktop/unitTests/gtest-demo/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("//home/bojan/Desktop/unitTests/gtest-demo/build/test/unit_tests[1]_include.cmake")
+subdirs("../_deps/gtest-build")
